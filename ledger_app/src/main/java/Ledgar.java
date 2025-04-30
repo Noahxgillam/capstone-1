764e70a94b2fd64b9daab2d0aa9bbb74fe6752e2
Noah Gillam <ngillam@appdev.yearup.org>
2025-04-30 15:36:59 -0500

    added fixed errors

--- ledger_app/src/main/java/Ledgar.java
+++ ledger_app/src/main/java/Ledgar.java
@@ -4,7 +4,6 @@ import java.sql.SQLOutput;
 
 public class Ledgar {
     public static void main(String[] args) {
-        if (inputD);
         try {
             FileInputStream fis = new FileInputStream("transactions.csv.txt");
         } catch (FileNotFoundException e) {
--- ledger_app/src/main/java/array.java
+++ ledger_app/src/main/java/array.java
@@ -23,6 +23,7 @@ public class array {
             }
 
 
+        return list;
     }
 
 
